343c07da0252c66684e9be2695538bd6c55bfb27
Aaryan Purohit <ap1922@ic.ac.uk>
2025-02-05 21:23:41 +0000

    (feat) Built pipeline to create Author and Grant objects from DOI of a paper, using PyAlex, GtR and ORCID APIs

--- ranking_model/author.py
+++ ranking_model/author.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Author:
-    def __init__(self, name, citations, dob, hindex):
+    def __init__(self, name, citations, dob, orcid, hindex, grants, org_name, website):
         """
         Represents an author.
         :param name: Author's name.
@@ -9,11 +9,15 @@ class Author:
         :param dob: Date of birth.
         :param hindex: h-index.
         """
+        self.website = website
         self.name = name
         self.citations = citations
         self.dob = dob
         self.hindex = hindex
         self.score = None  # This will be computed by the Ranker
+        self.grants = grants if grants else []
+        self.orcid = orcid
+        self.org_name = org_name
 
     def get_feature_vector(self):
         """Return a numpy array of features used for ranking (order: citations, hindex)."""
