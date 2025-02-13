cdc6373442b435b4401b1657dd552d68febe6e3e
aa6622 <aa6622@ic.ac.uk>
2025-02-13 20:45:05 +0000

    Chore: outdated function description

--- ranking_model/paper.py
+++ ranking_model/paper.py
@@ -2,15 +2,6 @@ class Paper:
     def __init__(self, paper_id, openalex_id, title, relevancy, authors, doi, abstract, publication_date):
         """
         Represents a paper.
-        :param paper_id: A unique identifier.
-        :param name: Name or short code for the paper.
-        :param title: Full title.
-        :param institution: Institution that produced the paper.
-        :param institution_size: Number of people at the institution.
-        :param funding: Amount of funding (e.g., in thousands).
-        :param citations: Number of citations for the paper.
-        :param relevancy: A precomputed relevancy score for the search topic.
-        :param authors: List of Author objects.
         """
         self.publication_date = publication_date
         self.abstract = abstract
