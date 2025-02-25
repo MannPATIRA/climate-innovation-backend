082d6df5c0aad300bad1cde5777d755369c49b91
aa6622 <aa6622@ic.ac.uk>
2025-02-25 15:35:38 +0000

    Fix: disable ranker.rank

--- backend_server/main.py
+++ backend_server/main.py
@@ -259,7 +259,8 @@ async def search_papers(query: PaperQuery):
                 authors=authors
             ))
 
-        return ranker.rank_papers(paper_results)
+        return paper_results
+        return ranker.ranker(paper_results)
         
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -274,10 +275,10 @@ async def paper_feedback(feedback: PaperFeedback):
     try:
         paper_obj = build_paper_from_dict(feedback.paper.model_dump())
         if feedback.accepted:
-            ranker.accept_paper(paper_obj)
+            # ranker.accept_paper(paper_obj)
             message = "Paper accepted. Model weights updated."
         else:
-            ranker.delete_paper(paper_obj)
+            # ranker.delete_paper(paper_obj)
             message = "Paper rejected. Model weights updated."
         # message = "Paper ranking is disabled"
         return {"message": message}
@@ -302,10 +303,10 @@ async def author_feedback(feedback: AuthorFeedback):
         if target_author is None:
             raise HTTPException(status_code=404, detail="Author not found in paper.")
         if feedback.accepted:
-            ranker.accept_author(paper_obj, target_author)
+            # ranker.accept_author(paper_obj, target_author)
             message = "Author accepted. Model weights updated."
         else:
-            ranker.delete_author(paper_obj, target_author)
+            # ranker.delete_author(paper_obj, target_author)
             message = "Author rejected. Model weights updated."
         # message = "Author ranking is disabled"
         return {"message": message}
